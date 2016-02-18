27b964d797925210d93c6dc6d1d7fd233f2fc62d
Oskar Stenman <oskar@cetex.se>
2016-02-18 12:22:51 +0100

    Refactor a bit so we sort template output by what router the config is going to.

--- demo.py
+++ demo.py
@@ -7,18 +7,33 @@ import os
 cfgrdr = awsVpnConfig(region='ap-southeast-1')
 config = cfgrdr.get()
 
-for index, vpn in enumerate(config):
-    print("VPN config: {}".format(index))
-    pprint(vpn)
-
+# Resort the vpn configs
+# We want to split the output by which router it's going to be used on.
+# So let's move the list of configs to a dict based on what router it's
+# connecting to.
+newConfig = {}
 for vpn in config:
     # Gets second octet "x" from CidrBlock 10.x.0.0/y
     # This is used for naming tunnel interfaces in templating
     vpn['NetId'] = vpn['CidrBlock'].split(".")[1]
+    
+    # A bit ugly, but both (all) CustomerGatewayConfigurations for 
+    # one vpn config are always going to the same Customer Gateway ip
+    # so we just need to grab any of them.
+    routerIP = vpn['CustomerGatewayConfiguration'][0]['cgw_tunnel_outside_address']
+    if routerIP not in newConfig:
+        newConfig[routerIP] = []
+    
+    # append routerip inside the vpn config, also not pretty but works
+    vpn['RouterIP'] = routerIP
+    newConfig[routerIP].append(vpn)
+    pprint(newConfig[routerIP])
+
 
 templates = ['myCisco']
 templatedir = os.path.realpath("./templates")
-ENV = Environment(loader=FileSystemLoader(templatedir))
-for tmplt in templates:
-    template = ENV.get_template(tmplt + ".j2")
-    print template.render(config=config)
+for key, value in newConfig.iteritems():
+    ENV = Environment(loader=FileSystemLoader(templatedir))
+    for tmplt in templates:
+        template = ENV.get_template(tmplt + ".j2")
+        print template.render(config=value)
